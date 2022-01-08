70b01793aa7758b7e47b11110ef460dada5007aa
keasar <shaokaikkk@163.com>
2022-01-08 23:41:21 +0800

    1.8

--- skfrontend/src/App.vue
+++ skfrontend/src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <Hello msg="Welcome to Your Vue.js App"/> -->
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <Hello msg="Welcome to Your Vue.js App"/> -->
     <p>
       <router-link to="/">首页</router-link>
       <router-link to="/news">新闻</router-link>
       <router-link to="/books">图书</router-link>
       <router-link to="/videos">视频</router-link>
-
     </p>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
+// import Hello from './components/Hello.vue'
 
 export default {
   name: 'App',
--- skfrontend/src/router/index.js
+++ skfrontend/src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '@/components/Home'
-import News from '@/components/News'
-import Books from '@/components/Books'
-import Videos from '@/components/Videos'
+import Home from "../components/Home"
+import News from "../components/News"
+import Books from "../components/Books"
+import Videos from "../components/Videos"
 
 const router = createRouter({
     history: createWebHashHistory(),
