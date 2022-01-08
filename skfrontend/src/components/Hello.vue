46f317d97f28284710d7927f56980ebeb7ff017f
keasar <shaokaikkk@163.com>
2022-01-08 21:35:02 +0800

    1.8

---
+++ skfrontend/src/components/Hello.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>{{message}}</p>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                message: 'hello, Vue.js'
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
